1547889185cf73387620911c6182d4df5a5e0c30
Andrew <andrusha.kolmakov@yandex.ru>
2025-04-09 12:21:28 +0300

    add redis cache

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -9,15 +9,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\WeatherCityRequest;
 use App\Services\Interfaces\WeatherServiceInterface;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class WeatherController extends Controller
 {
     public function __invoke(WeatherCityRequest $request, WeatherServiceInterface $service): JsonResponse
     {
-        return new JsonResponse(
-            $service->getWeatherByCity(
-                new WeatherRequestDTO($request->validated('city'))
-            )
-        );
+        $city = $request->validated('city');
+
+        return Cache::remember($city, 3600, function () use ($service, $city) {
+            return new JsonResponse(
+                $service->getWeatherByCity(
+                    new WeatherRequestDTO($city)
+                )
+            );
+        });
     }
 }
